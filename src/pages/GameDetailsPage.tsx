fbb27e952c5e69883df95db0ff6b16a21ee329ab
Roy <carrerachristianroy@gmail.com>
2024-06-13 14:46:54 +0800

    Handle Errors (page and ui)

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,8 +1,12 @@
+import { Box } from "@chakra-ui/react"
 
 const GameDetailsPage = () => {
     return(
         <>
+        <Box padding={5}>
+
         <p>GAME DETAILS PAGE</p>
+        </Box>
         </>
     )
 }
